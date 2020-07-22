6739b91c6224702fc9fa2048118503f07e43bbf2
dragon <trrstnm@gmail.com>
2020-07-22 13:13:26 +0800

    Initializes code cult elixir problem

---
+++ lib/rate_card.ex
@@ -0,0 +1,18 @@
+defmodule RateCard do
+  @moduledoc """
+  Documentation for `RateCard`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RateCard.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rate_card_test.exs
@@ -0,0 +1,8 @@
+defmodule RateCardTest do
+  use ExUnit.Case
+  doctest RateCard
+
+  test "greets the world" do
+    assert RateCard.hello() == :world
+  end
+end
