eece23bb5ca133dd65af487def6373dc0ec41a8c
dragon <trrstnm@gmail.com>
2020-07-22 19:09:41 +0800

    Changes wrong vehicle types in test

--- test/rate_card_test.exs
+++ test/rate_card_test.exs
@@ -4,23 +4,23 @@ defmodule RateCardTest do
 
   describe "Rate card pricing" do
     test "vehicle car returns correct price" do
-      assert RateCard.get_pricing_for("car", -0.1) == {:error, :invalid_distance}
-      assert RateCard.get_pricing_for("car", 0.1) == {:ok, 15}
-      assert RateCard.get_pricing_for("car", 5.1) == {:ok, 17}
-      assert RateCard.get_pricing_for("car", 10.1) == {:ok, 22}
-      assert RateCard.get_pricing_for("car", 15.1) == {:ok, 27}
-      assert RateCard.get_pricing_for("car", 20) == {:ok, 30}
-      assert RateCard.get_pricing_for("car", 100) == {:ok, 30}
+      assert RateCard.get_pricing_for("bike", -0.1) == {:error, :invalid_distance}
+      assert RateCard.get_pricing_for("bike", 0.1) == {:ok, 15}
+      assert RateCard.get_pricing_for("bike", 5.1) == {:ok, 17}
+      assert RateCard.get_pricing_for("bike", 10.1) == {:ok, 22}
+      assert RateCard.get_pricing_for("bike", 15.1) == {:ok, 27}
+      assert RateCard.get_pricing_for("bike", 20) == {:ok, 30}
+      assert RateCard.get_pricing_for("bike", 100) == {:ok, 30}
     end
 
     test "vehicle bike returns correct price" do
-      assert RateCard.get_pricing_for("bike", -0.1) == {:error, :invalid_distance}
-      assert RateCard.get_pricing_for("bike", 0.1) == {:ok, 20}
-      assert RateCard.get_pricing_for("bike", 5.1) == {:ok, 25}
-      assert RateCard.get_pricing_for("bike", 10.1) == {:ok, 30}
-      assert RateCard.get_pricing_for("bike", 15.1) == {:ok, 35}
-      assert RateCard.get_pricing_for("bike", 20) == {:ok, 40}
-      assert RateCard.get_pricing_for("bike", 100) == {:ok, 40}
+      assert RateCard.get_pricing_for("car", -0.1) == {:error, :invalid_distance}
+      assert RateCard.get_pricing_for("car", 0.1) == {:ok, 20}
+      assert RateCard.get_pricing_for("car", 5.1) == {:ok, 25}
+      assert RateCard.get_pricing_for("car", 10.1) == {:ok, 30}
+      assert RateCard.get_pricing_for("car", 15.1) == {:ok, 35}
+      assert RateCard.get_pricing_for("car", 20) == {:ok, 40}
+      assert RateCard.get_pricing_for("car", 100) == {:ok, 40}
     end
   end
 end
